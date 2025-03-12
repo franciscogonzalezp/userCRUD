cb777b74f373b7cba94f98884241713170e48256
fanciscogonzalezp <franciscogonzalezparra0@gmail.com>
2025-03-12 07:35:48 +0100

    Componente home funcional y maquetado basicamente

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { IUser } from '../../interfaces/iuser.interface';
+import { CardUserComponent } from "../../components/card-user/card-user.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardUserComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  usersList: IUser[] = []
+  userService: UserService = inject(UserService);
+
+  getUsers() {
+    this.userService.getAllUsers().subscribe({
+      next: response => {
+        this.usersList = response.results
+      },
+      error: error => {
+        console.log("Error", error)
+      }
+    })
+  }
+
+  ngOnInit(){
+    this.getUsers();
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { IResponse } from '../interfaces/iresponse.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUser } from '../interfaces/iuser.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private endpoint: string = "https://peticiones.online/api/users"
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  getAllUsers(): Observable<IResponse> {
+    return this.httpClient.get<IResponse>(this.endpoint);
+  }
+
+  getUser(id: number): Observable<IUser> {
+    return this.httpClient.get<IUser>(`${this.endpoint}/${id}`)
+  }
+
+  createUser(body: IUser): Observable<IUser> {
+    return this.httpClient.post<IUser>(this.endpoint, body)
+  }
+
+  deleteUser(id: number): Observable<IUser> {
+    return this.httpClient.delete<IUser>(`${this.endpoint}/${id}`)
+  }
+
+  updateUser(body: IUser): Observable<IUser> {
+    return this.httpClient.put<IUser>(`${this.endpoint}/${body.id}`, body)
+  }
 }
